8fec45b595eac6a1d5eceebc213178920199dc0c
yejijang <yeji0773@naver.com>
2020-04-03 21:14:20 +0900

    3주 과제_2

--- week03/vowel.js
+++ week03/vowel.js
@@ -1,6 +1,9 @@
 //"When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
 //위 문자열에서 모음(aeiou)들의 위치를 출력하는 코드를 구현하시오.
 
-let s = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+let s = "When I find myself in times of trouble Mother MAry comes to me Speaking words of wisdom, let it be.";
 
-console.log(s.search(/[aeiou]/ig)); //--> 한 개의 값만 출력됨,,
+for (let i = 0; i < s.length; i++) {
+  if (s[i].search(/[aeiou]/i) != -1)
+  console.log(i);
+}
